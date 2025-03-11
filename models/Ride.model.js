e6e7f4ae988da0aa0e475b3f540c6bacddd3b406
Anjana Hariprasad <anjanahariprasad21@gmail.com>
2025-03-11 21:08:41 +0100

    Add additional input fields to ride model.

--- models/Ride.model.js
+++ models/Ride.model.js
@@ -1,35 +1,37 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const rideSchema = new Schema({
+const rideSchema = new Schema(
+  {
     origin: {
-        type: String,
-        required: true,
-      },
-      destination: {
-        type: String,
-        required: true,
-      },
-      seatsAvailable: {
-        type: Number,
-        required: true,
-        min: 1,
-      },
-      price: {
-        type: Number,
-        required: true,
-        min: 0,
-      },
-      travelDate: {type: Date},
-      StartTime: { type: String },
-      EndTime:{type: String},
-      driverId: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-      },
-    }, { timestamps: true });
+      type: String,
+      required: true,
+    },
+    destination: {
+      type: String,
+      required: true,
+    },
+    seatsAvailable: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    travelDate: { type: String },
+    startTime: { type: String },
+    endTime: { type: String },
+    distance: {type: String},
+    driverId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = model("Ride", rideSchema);
-
-

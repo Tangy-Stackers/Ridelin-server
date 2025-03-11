20987612d110de20cdc06bd29f28520a960041a4
Anjana Hariprasad <anjanahariprasad21@gmail.com>
2025-03-11 21:11:53 +0100

    Modify user update

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const User = require("../models/User.model");
 
 const { isAuthenticated } = require("../middleware/jwt.middleware.js");
-
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 //GET/user-----> get user
-router.get("/",isAuthenticated, (req, res, next) => {
+router.get("/", isAuthenticated, (req, res, next) => {
   User.find({})
     .populate("user")
     .then((users) => {
@@ -14,11 +15,11 @@ router.get("/",isAuthenticated, (req, res, next) => {
     })
     .catch((error) => {
       console.log(error);
-      res.status(500).json({ message: "Server Error",error });
+      res.status(500).json({ message: "Server Error", error });
     });
 });
 // GET/userId------> get a specific user by ID
-router.get("/:userId",isAuthenticated, (req, res) => {
+router.get("/:userId", isAuthenticated, (req, res) => {
   const { userId } = req.params;
   User.findById(userId)
     // .populate("userId")
@@ -27,48 +28,39 @@ router.get("/:userId",isAuthenticated, (req, res) => {
     })
     .catch((error) => {
       console.log(error);
-      res.status(500).json({ message: "Server Error",error });
+      res.status(500).json({ message: "Server Error", error });
     });
 });
 // PATCH/userId-------> Update user info
-router.patch("/:userId",isAuthenticated, (req, res) => {
+router.patch("/:userId", isAuthenticated, (req, res) => {
   const { userId } = req.params;
-  const { password } = req.body;
+  const { password, email, name, phone, image, about } = req.body;
+  const updateFields = {};
 
-  User.findById(userId)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).json({ message: "User not found" });
+  User.findById(userId).then((user) => {
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }    
+    if (name) updateFields.name = name;
+    if (phone) updateFields.phone = phone;
+    if (image) updateFields.image = image;
+    if (about) updateFields.about = about;
+
+    User.findByIdAndUpdate(userId, updateFields, { new: true })
+      .then((updatedUser) => {
+        if (!updatedUser) {
+          res.status(404).json({ message: "User not found" });
+          return;
+        }else{
+        res.status(200).json({message: "User updated successfully", updatedUser});
+        return;
       }
-      // Check the users collection if a user with the same email already exists
-      const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
-        if (!passwordRegex.test(password)) {
-            res.status(400).json({
-            message:"Password must have at least 8 characters and contain at least one number, one lowercase and one uppercase letter.",
-            });
-            return;
-        }
-    // If password is unique, proceed to hash the password
-          const salt = bcrypt.genSaltSync(saltRounds);
-          const hashedPassword = bcrypt.hashSync(password, salt);
-      return User.findByIdAndUpdate(
-        userId,
-        { password: hashedPassword},
-        { new: true }
-      );
-      
-    })
-    .then((updatedBooking) => {
-      res.status(200).json({
-        message: "Booking updated successfully",
-        booking: updatedBooking,
+      })
+      .catch((error) => {
+        res.status(500).json({ message: "Server error", error });
+        return;
       });
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).json({ message: "Server error",error });
-    });
+  });
 });
-
 
 module.exports = router;
